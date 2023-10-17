44a08e8404b0188ccfa4bcafd6829dd140f88a86
rayhanalmim <rayhanalmim1@gmail.com>
2023-10-17 23:09:03 +0600

    created json file for show brand in homepage and also added Add product UI

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -4,6 +4,7 @@ import Home from "../Component/Root/Home";
 import SingIn from "../Component/Authentication/SingIn";
 import SingUp from "../Component/Authentication/SingUp";
 import ErrorPage from "../Component/Root/ErrorPage";
+import AddProduct from "../Other Component/AddProduct";
 
 const Layout = createBrowserRouter([
     {
@@ -13,7 +14,8 @@ const Layout = createBrowserRouter([
         children: [
             {
                 path:'/',
-                element:<Home></Home>
+                element:<Home></Home>,
+                loader: () => fetch('/brandinfo.json')
             },
             {
                 path:'/login',
@@ -22,6 +24,10 @@ const Layout = createBrowserRouter([
             {
                 path:'/registration',
                 element:<SingUp></SingUp>
+            },
+            {
+                path:'/add',
+                element:<AddProduct></AddProduct>
             },
         ]
     }
---
+++ src/Other Component/AddProduct.jsx
@@ -0,0 +1,81 @@
+import Swal from "sweetalert2";
+
+const AddProduct = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const productName = form.productName.value;
+        const category = form.category.value;
+        const company = form.company.value;
+        const price = form.price.value;
+        const photoUrl = form.url.value;
+        const rating = form.number.value;
+        const productDetails = form.about.value;
+
+        const data = { productName, category, company, price, photoUrl, rating, productDetails };
+
+        console.log(data)
+        fetch('https://admit-dashboard-server-kjp82kj0d-rayhan-al-mims-projects.vercel.app/product', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+                if (result.acknowledged) {
+                    Swal.fire(
+                        'Congratulation!',
+                        'Product added successfully!',
+                        'success'
+                    )
+                    form.reset();
+                }
+            })
+    }
+
+    return (
+        <div className="w-11/12 mx-auto">
+            <h3 className=" text-center font-bold text-3xl">Add Product</h3>
+            <form onSubmit={handleSubmit}>
+                <div className="grid gap-6 mt-4 mb-6 md:grid-cols-2">
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product name</label>
+                        <input name='productName' type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="product name" required />
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Type of product</label>
+                        <input type="text" name='category' id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Category" required />
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Brand Name</label>
+                        <input type="text" name='company' id="company" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="company" required />
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                        <input type="number" id="price" name='price' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="$Price" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product photo URL</label>
+                        <input type="url" name='url' id="website" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="photoUrl" required />
+                    </div>
+                    <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rating</label>
+                        <input type="number" name='number' id="visitors" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Rating" required />
+                    </div>
+                </div>
+                <div className="mb-6">
+                    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">About product</label>
+                    <input type="text" id="text" name="about" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="details" required />
+                </div>
+                <button type="submit" className="bg-gray-600 hover:bg-gray-900 border text-white text-lg font-semibold  rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com">Submit</button>
+
+            </form>
+        </div>
+    );
+};
+
+export default AddProduct;
